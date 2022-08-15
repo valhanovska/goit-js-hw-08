181041378d1a5ba3249e98c4e394216433d306f2
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-15 22:31:20 +0300

    add task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function createGallery() {
+  let imgCard = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+  galleryEl.innerHTML = imgCard;
+}
+createGallery();
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
