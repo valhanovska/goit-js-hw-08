362deb67906343695e6c50378abce62c661c7c8a
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-15 22:31:33 +0300

    add task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import videoPlayer from '@vimeo/player';
+import lodashThrottle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new videoPlayer(iframe);
+
+player.on(
+  'timeupdate',
+  lodashThrottle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }),
+  1000
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
