a29f2188cf24a065959d78175568aeb30e358278
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-15 22:31:53 +0300

    add task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('[name="email"]');
+const textareaEl = document.querySelector('[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+formEl.addEventListener('submit', onFormSubmit);
+
+feedbackForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+  event.currentTarget.reset();
+  if (textareaEl.value === '' && emailEl.value === '')
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  formData = {
+    email: emailEl.value,
+    message: textareaEl.value,
+  };
+  console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function feedbackForm() {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (formData) {
+    textareaEl.value = formData.message;
+    emailEl.value = formData.email;
+  }
+}
